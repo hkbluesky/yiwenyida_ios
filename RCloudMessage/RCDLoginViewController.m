b69322c20473eb30d5872791b0b93a8f1d623e0d
Chris Law <690067513@qq.com>
2018-03-28 15:56:15 +0800

    修改UI

--- RCloudMessage/RCDLoginViewController.m
+++ RCloudMessage/RCDLoginViewController.m
@@ -177,7 +177,7 @@ MBProgressHUD *hud;
   //_account.placeholder=[NSString stringWithFormat:@"Email"];
   UIColor *color = [UIColor blackColor];
   userNameTextField.attributedPlaceholder = [[NSAttributedString alloc]
-      initWithString:NSLocalizedStringFromTable(@"ID", @"RongCloudKit",nil)
+      initWithString:NSLocalizedStringFromTable(@"Phone Number", @"RongCloudKit",nil)
           attributes:@{NSForegroundColorAttributeName : color}];
   userNameTextField.textColor = [UIColor blackColor];
   
